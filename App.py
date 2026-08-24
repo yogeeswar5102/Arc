b9ffc96d88961705e5c27ddb376a7b181622c69d
Yogeeswar <yogeeswarbharath5102@gmail.com>
2026-08-24 11:22:41 +0530

    Configure Vercel Python entrypoint

--- App.py
+++ App.py
@@ -9,7 +9,7 @@ HTML = """
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    <title>For Tamil</title>
+    <title>For Tamilselvi</title>
 
     <style>
         * {
